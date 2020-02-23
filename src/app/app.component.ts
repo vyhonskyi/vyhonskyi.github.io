7f69e0e4a5a1b4f4581442725383e203b44d91b0
Vitalii Vyhonskyi <v.vyhonskyi@gmail.com>
2020-02-23 13:32:59 +0200

    Education & Skills sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  template: `    
-    
-  `,
-  styles: []
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 }

7b288dbd7caeb76c5919642ea78179e9052cca84
Vitalii Vyhonskyi <v.vyhonskyi@gmail.com>
2020-02-23 14:56:59 +0200

    navbar toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { trigger, state, transition, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('navbarCollapsedExpanded', [
+      state('collapsed', style({
+        display: 'none'
+      })),
+      state('expanded', style({
+        display: 'block'
+      }))
+    ])
+  ]
 })
 export class AppComponent {
+  public navbarExpanded: boolean;
+
+  public toggleNavbar(): void {
+    this.navbarExpanded = !this.navbarExpanded;
+  }
 }
